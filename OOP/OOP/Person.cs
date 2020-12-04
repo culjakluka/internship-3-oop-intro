f096a92be97a2652b5256f49ac91869d80159d8b
culjakluk <llukaculjak@gmail.com>
2020-12-04 20:31:49 +0100

    added person and event classes

---
+++ OOP/OOP/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, long oIB, long phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            OIB = oIB;
+            PhoneNumber = phoneNumber;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long OIB { get; set; }
+        public long PhoneNumber { get; set; }
+    }
+}
