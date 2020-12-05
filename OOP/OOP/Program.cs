a888c943970ac5db613c206177515c7104ebb754
culjakluka <llukaculjak@gmail.com>
2020-12-05 16:14:26 +0100

    added enum

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 
 namespace OOP
 {
+    public enum _EventType
+    {
+        Cofee,
+        Lecture,
+        Concert,
+        StudySession
+    }
     class Program
     {
         static void Main(string[] args)
         {
             var dict = new Dictionary<Event, List<Person>>();
+
         }
         static string Input(string message)
         {
