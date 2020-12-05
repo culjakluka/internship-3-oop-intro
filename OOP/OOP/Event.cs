831a6a03c6c53f07e5e86955027348bf3a302906
culjakluka <llukaculjak@gmail.com>
2020-12-05 21:51:20 +0100

    added printing method to event class

--- OOP/OOP/Event.cs
+++ OOP/OOP/Event.cs
@@ -18,5 +18,14 @@ namespace OOP
         public _EventType EventType { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
+        public void PrintEvent() 
+        {
+            Console.WriteLine("===================================");
+            Console.WriteLine("Event name: " + Name);
+            Console.WriteLine("Event type: " + (_EventType)EventType);
+            Console.WriteLine("Event start time: " + StartTime.ToUniversalTime());
+            Console.WriteLine("Event end time: " + EndTime.ToUniversalTime());
+            Console.WriteLine("===================================");
+        }
     }
 }
